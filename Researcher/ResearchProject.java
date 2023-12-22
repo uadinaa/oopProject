604014470a04affdb5f24573170461a1ea521340
AzamatTaubayev <116373076+AzamatTaubayev@users.noreply.github.com>
2023-12-22 20:44:18 +0600

    Update ResearchProject.java

--- Researcher/ResearchProject.java
+++ Researcher/ResearchProject.java
@@ -2,38 +2,40 @@ package Researcher;
 
 import java.util.*;
 
-public class ResearchProject extends ResearchPaper implements Comparable<ResearchPaper>{
+public class ResearchProject extends ResearchPaper{
 
 	private String title;
 
-	private ResearchPaper papers;
-
-	public Vector<ResearchPaper> allPublishedPapers;
-	public Vector<ResearchPaper> getAllPublishedPapers(){
-		return allPublishedPapers;
-	}
-
-	public void setAllPublishedPapers(Vector<ResearchPaper> allPublishedPapers) {
-		this.allPublishedPapers = allPublishedPapers;
-	}
-
-	private Vector<Researcher> authors;
-
+//	private Vector<Researcher> authors;
 	private Researcher supervisor;
+	public List<ResearchPaper> publishedPapers;
 	public ResearchProject(){
 
 		super();
 	}
-
-	@Override
-	public int compareTo(ResearchPaper o) {
-		if(title.length() == o.getTitle().length()){
-			return 0;
-		}
-		if(title.length() < o.getTitle().length()){
-			return -1;
-		}
-		return 1;
+	public ResearchProject(String title, Researcher supervisor, List<ResearchPaper> publishedPapers) {
+		this.title = title;
+		this.supervisor = supervisor;
+		this.publishedPapers = publishedPapers;
 	}
+
+	public List<ResearchPaper> getAllPublishedPapers(){
+
+		return publishedPapers;
+	}
+	public void addPublishedPapers(ResearchPaper p){
+		publishedPapers.add(p);
+	}
+
+//	@Override
+//	public int compareTo(ResearchPaper o) {
+//		if(title.length() == o.getTitle().length()){
+//			return 0;
+//		}
+//		if(title.length() < o.getTitle().length()){
+//			return -1;
+//		}
+//		return 1;
+//	}
 }
 
