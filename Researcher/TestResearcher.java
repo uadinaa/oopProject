5aabe52666c19925ba3f5b47df0e5b4d24953f2e
AzamatTaubayev <116373076+AzamatTaubayev@users.noreply.github.com>
2023-12-25 18:35:25 +0600

    Another Update TestResearcher.java

--- Researcher/TestResearcher.java
+++ Researcher/TestResearcher.java
@@ -9,40 +9,35 @@ public class TestResearcher {
 
     public static void main(String[] args) {
         // Create a sample researcher
-         Researcher researcher = new Researcher();
+        Researcher researcher = new Researcher();
 
         // Create some research papers
-        ResearchPaper paper1 = new ResearchPaper();
-        paper1.setTitle("Paper 1");
-        paper1.setAuthors(new Vector<>()); // add authors
-        paper1.setJournal("Journal A");
-        paper1.setPages(10);
-        paper1.setDate(new Date());
-        paper1.setDoi("doi123");
-        paper1.setCitations(5);
+        ResearchPaper paper1 = new ResearchPaper("Paper 15", 15, new Date(), "24555", 20);
+//        paper1.setTitle("Paper 1");
+//        paper1.setAuthors(new Vector<>()); // add authors
+//        paper1.setJournal("Journal A");
+//        paper1.setPages(10);
+//        paper1.setDate(new Date());
+//        paper1.setDoi("doi123");
+//        paper1.setCitations(5);
 
-        ResearchPaper paper2 = new ResearchPaper();
-        paper2.setTitle("Paper 2");
-        paper2.setAuthors(new Vector<>()); // add authors
-        paper2.setJournal("Journal B");
-        paper2.setPages(8);
-        paper2.setDate(new Date());
-        paper2.setDoi("doi456");
-        paper2.setCitations(8);
-
+        ResearchPaper paper2 = new ResearchPaper("Paper 2", 8, new Date(), "35000", 15);
+//        paper2.setTitle("Paper 2");
+//        paper2.setAuthors(new Vector<>()); // add authors
+//        paper2.setJournal("Journal B");
+//        paper2.setPages(8);
+//        paper2.setDate(new Date());
+//        paper2.setDoi("doi456");
+//        paper2.setCitations(8);
+        ResearchPaper paper3 = new ResearchPaper("Paper 3", 8, new Date(), "35000", 2);
         // Add research papers to the researcher
         researcher.addResearchPaper(paper1);
         researcher.addResearchPaper(paper2);
+        researcher.addResearchPaper(paper3);
 
         // Print papers sorted by title length
         System.out.println("Papers sorted by title length:");
         researcher.printPapers(new TitleLengthComparator());
+        System.out.println(researcher.calculateHIndex());
     }
 }
-//
-//public class TestResearcher {
-//    public static void main(String[] args){
-//        Vector<Researcher>authors = new Vector<Researcher>();
-//        Vector<ResearchPaper>
-//    }
-//
