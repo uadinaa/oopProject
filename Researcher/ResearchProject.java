18176e0b505bf7d31637442ed3e516a3cf4e41fc
AzamatTaubayev <116373076+AzamatTaubayev@users.noreply.github.com>
2023-12-26 16:02:43 +0600

    Update ResearchProject.java

--- Researcher/ResearchProject.java
+++ Researcher/ResearchProject.java
@@ -2,7 +2,7 @@ package Researcher;
 
 import java.util.*;
 
-public class ResearchProject extends ResearchPaper{
+public class ResearchProject{
 
 	private String title;
 
@@ -10,13 +10,19 @@ public class ResearchProject extends ResearchPaper{
 	private Researcher supervisor;
 	public List<ResearchPaper> publishedPapers;
 	public ResearchProject(){
-
-		super();
 	}
-	public ResearchProject(String title, Researcher supervisor, List<ResearchPaper> publishedPapers) {
+	public ResearchProject(String title, List<ResearchPaper> publishedPapers) {
 		this.title = title;
-		this.supervisor = supervisor;
 		this.publishedPapers = publishedPapers;
+	}
+
+	public void setSupervisor(Researcher supervisor) {
+		if(supervisor.calculateHIndex() >= 3){
+			this.supervisor = supervisor;
+		}
+		else{
+			System.out.println("H-index is less than 3 choose other supervisor");
+		}
 	}
 
 	public List<ResearchPaper> getAllPublishedPapers(){
@@ -24,18 +30,16 @@ public class ResearchProject extends ResearchPaper{
 		return publishedPapers;
 	}
 	public void addPublishedPapers(ResearchPaper p){
+
 		publishedPapers.add(p);
 	}
-
-//	@Override
-//	public int compareTo(ResearchPaper o) {
-//		if(title.length() == o.getTitle().length()){
-//			return 0;
-//		}
-//		if(title.length() < o.getTitle().length()){
-//			return -1;
-//		}
-//		return 1;
-//	}
+	@Override
+	public String toString() {
+		return "ResearchProject{" +
+				"title='" + title + '\'' +
+				", supervisor=" + supervisor +
+				", publishedPapers=" + publishedPapers +
+				'}';
+	}
 }
 
