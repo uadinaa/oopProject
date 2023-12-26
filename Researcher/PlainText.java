f743e2dd66d248d9731577afeffc2fa2dccd28a4
AzamatTaubayev <116373076+AzamatTaubayev@users.noreply.github.com>
2023-12-26 16:03:16 +0600

    Update PlainText.java

--- Researcher/PlainText.java
+++ Researcher/PlainText.java
@@ -2,9 +2,10 @@ package Researcher;
 
 import java.util.*;
 
-public class PlainText extends ResearchPaper implements Format{
+public class PlainText implements Format{
+
     @Override
     public String getCitation(String title, Vector<Researcher> authors, String journal, int pages, Date date, String doi) {
-        return "This is plain text formatting:" + getTitle() + ", " + getAuthors() + ", " + getJournal() + ", " + getPages() + ", " + getDate() + ", doi: " + getDoi();
+        return "This is plain text formatting:" + title + ", " + authors + ", " + journal + ", " + pages + ", " + date + ", doi: " + doi;
     }
 }
