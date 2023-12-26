9a50bf11db88576aba895793c90ebd74cef0ee02
AzamatTaubayev <116373076+AzamatTaubayev@users.noreply.github.com>
2023-12-26 16:03:40 +0600

    Update BibTex.java

--- Researcher/BibTex.java
+++ Researcher/BibTex.java
@@ -2,11 +2,10 @@ package Researcher;
 
 import java.util.*;
 
-public class BibTex extends ResearchPaper implements Format{
-
+public class BibTex implements Format{
 
     @Override
     public String getCitation(String title, Vector<Researcher> authors, String journal, int pages, Date date, String doi) {
-        return "{author = {" + getAuthors() + "}, \n" + "title = {" + getTitle() + "}, \n" + "date = {" + getDate() + "}, \n" + "doi = {" + getDoi() + "}}";
+        return "{author = {" + authors + "}, \n" + "title = {" + title + "}, \n" + "date = {" + date + "}, \n" + "doi = {" + doi + "}}";
     }
 }
